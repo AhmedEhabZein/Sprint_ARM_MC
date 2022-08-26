ab3b71d7e3edf00db3e36aab54b65f0109e1e0e9
AhmedEhabZein <ahmedehabzein@gmail.com>
2022-08-26 16:26:40 +0200

    second commit

--- Mcal/Dio.c
+++ Mcal/Dio.c
@@ -1,1 +1,328 @@
-.
+ /******************************************************************************
+ *
+ * Module: Dio
+ *
+ * File Name: Dio.c
+ *
+ * Description: Source file for TM4C123GH6PM Microcontroller - Dio Driver
+ *
+ * Author: Ahmed Ehab
+ ******************************************************************************/
+
+#include "Dio.h"
+#include "Dio_Regs.h"
+
+
+STATIC const Dio_ConfigChannel * Dio_PortChannels = NULL_PTR;
+STATIC uint8 Dio_Status = DIO_NOT_INITIALIZED;
+
+/************************************************************************************
+* Service Name: Dio_Init
+* Sync/Async: Synchronous
+* Reentrancy: Non reentrant
+* Parameters (in): ConfigPtr - Pointer to post-build configuration data
+* Parameters (inout): None
+* Parameters (out): None
+* Return value: None
+* Description: Function to Initialize the Dio module.
+************************************************************************************/
+void Dio_Init(const Dio_ConfigType * ConfigPtr)
+{
+
+	if (NULL_PTR == ConfigPtr)
+	{
+	
+	}
+	else
+	{
+		/*
+		 * Set the module state to initialized and point to the PB configuration structure using a global pointer.
+		 * This global pointer is global to be used by other functions to read the PB configuration structures
+		 */
+		Dio_Status       = DIO_INITIALIZED;
+		Dio_PortChannels = ConfigPtr->Channels; /* address of the first Channels structure --> Channels[0] */
+	}
+}
+
+/************************************************************************************
+* Service Name: Dio_WriteChannel
+* Sync/Async: Synchronous
+* Reentrancy: Reentrant
+* Parameters (in): ChannelId - ID of DIO channel.
+*                  Level - Value to be written.
+* Parameters (inout): None
+* Parameters (out): None
+* Return value: None
+* Description: Function to set a level of a channel.
+************************************************************************************/
+void Dio_WriteChannel(Dio_ChannelType ChannelId, Dio_LevelType Level)
+{
+	volatile uint32 * Port_Ptr = NULL_PTR;
+	boolean error = FALSE;
+
+	/* Check if the Driver is initialized before using this function */
+	if (DIO_NOT_INITIALIZED == Dio_Status)
+	{
+		error = TRUE;
+	}
+
+	/* Check if the used channel is within the valid range */
+	if (DIO_CONFIGURED_CHANNLES <= ChannelId)
+	{
+		error = TRUE;
+	}
+
+	/* In-case there are no errors */
+	if(FALSE == error)
+	{
+		/* Point to the correct PORT register according to the Port Id stored in the Port_Num member */
+		switch(Dio_PortChannels[ChannelId].Port_Num)
+		{
+            case 0:    Port_Ptr = &GPIO_PORTA_DATA_REG;
+		               break;
+		    case 1:    Port_Ptr = &GPIO_PORTB_DATA_REG;
+		               break;
+		    case 2:    Port_Ptr = &GPIO_PORTC_DATA_REG;
+		               break;
+		    case 3:    Port_Ptr = &GPIO_PORTD_DATA_REG;
+		               break;
+            case 4:    Port_Ptr = &GPIO_PORTE_DATA_REG;
+		               break;
+            case 5:    Port_Ptr = &GPIO_PORTF_DATA_REG;
+		               break;
+		}
+		if(Level == STD_HIGH)
+		{
+			/* Write Logic High */
+			SET_BIT(*Port_Ptr,Dio_PortChannels[ChannelId].Ch_Num);
+		}
+		else if(Level == STD_LOW)
+		{
+			/* Write Logic Low */
+			CLEAR_BIT(*Port_Ptr,Dio_PortChannels[ChannelId].Ch_Num);
+		}
+	}
+}
+
+/************************************************************************************
+* Service Name: Dio_ReadChannel
+* Sync/Async: Synchronous
+* Reentrancy: Reentrant
+* Parameters (in): ChannelId - ID of DIO channel.
+* Parameters (inout): None
+* Parameters (out): None
+* Return value: Dio_LevelType
+* Description: Function to return the value of the specified DIO channel.
+************************************************************************************/
+Dio_LevelType Dio_ReadChannel(Dio_ChannelType ChannelId)
+{
+	volatile uint32 * Port_Ptr = NULL_PTR;
+	Dio_LevelType output = STD_LOW;
+	boolean error = FALSE;
+
+	/* Check if the Driver is initialized before using this function */
+	if (DIO_NOT_INITIALIZED == Dio_Status)
+	{
+		error = TRUE;
+	}
+	
+	/* Check if the used channel is within the valid range */
+	if (DIO_CONFIGURED_CHANNLES <= ChannelId)
+	{
+		error = TRUE;
+	}
+
+	/* In-case there are no errors */
+	if(FALSE == error)
+	{
+		/* Point to the correct PORT register according to the Port Id stored in the Port_Num member */
+		switch(Dio_PortChannels[ChannelId].Port_Num)
+		{
+            case 0:    Port_Ptr = &GPIO_PORTA_DATA_REG;
+		               break;
+		    case 1:    Port_Ptr = &GPIO_PORTB_DATA_REG;
+		               break;
+		    case 2:    Port_Ptr = &GPIO_PORTC_DATA_REG;
+		               break;
+		    case 3:    Port_Ptr = &GPIO_PORTD_DATA_REG;
+		               break;
+            case 4:    Port_Ptr = &GPIO_PORTE_DATA_REG;
+		               break;
+            case 5:    Port_Ptr = &GPIO_PORTF_DATA_REG;
+		               break;
+		}
+		/* Read the required channel */
+		if(BIT_IS_SET(*Port_Ptr,Dio_PortChannels[ChannelId].Ch_Num))
+		{
+			output = STD_HIGH;
+		}
+		else
+		{
+			output = STD_LOW;
+		}
+	}
+        return output;
+}
+
+/************************************************************************************
+* Service Name: Dio_FlipChannel
+* Sync/Async: Synchronous
+* Reentrancy: Reentrant
+* Parameters (in): ChannelId - ID of DIO channel.
+* Parameters (inout): None
+* Parameters (out): None
+* Return value: Dio_LevelType
+* Description: Function to flip the level of a channel and return the level of the channel after flip.
+************************************************************************************/
+Dio_LevelType Dio_FlipChannel(Dio_ChannelType ChannelId)
+{
+	volatile uint32 * Port_Ptr = NULL_PTR;
+	Dio_LevelType output = STD_LOW;
+	boolean error = FALSE;
+
+	/* Check if the Driver is initialized before using this function */
+	if (DIO_NOT_INITIALIZED == Dio_Status)
+	{
+		error = TRUE;
+	}
+
+	/* Check if the used channel is within the valid range */
+	if (DIO_CONFIGURED_CHANNLES <= ChannelId)
+	{
+		error = TRUE;
+	}
+
+	/* In-case there are no errors */
+	if(FALSE == error)
+	{
+		/* Point to the correct PORT register according to the Port Id stored in the Port_Num member */
+		switch(Dio_PortChannels[ChannelId].Port_Num)
+		{
+            case 0:    Port_Ptr = &GPIO_PORTA_DATA_REG;
+		               break;
+		    case 1:    Port_Ptr = &GPIO_PORTB_DATA_REG;
+		               break;
+		    case 2:    Port_Ptr = &GPIO_PORTC_DATA_REG;
+		               break;
+		    case 3:    Port_Ptr = &GPIO_PORTD_DATA_REG;
+		               break;
+            case 4:    Port_Ptr = &GPIO_PORTE_DATA_REG;
+		               break;
+            case 5:    Port_Ptr = &GPIO_PORTF_DATA_REG;
+		               break;
+		}
+		/* Read the required channel and write the required level */
+		if(BIT_IS_SET(*Port_Ptr,Dio_PortChannels[ChannelId].Ch_Num))
+		{
+			CLEAR_BIT(*Port_Ptr,Dio_PortChannels[ChannelId].Ch_Num);
+			output = STD_LOW;
+		}
+		else
+		{
+			SET_BIT(*Port_Ptr,Dio_PortChannels[ChannelId].Ch_Num);
+			output = STD_HIGH;
+		}
+	}
+        return output;
+}
+
+/************************************************************************************
+* Service Name: Dio_WritePort
+* Sync/Async: Synchronous
+* Reentrancy: Reentrant
+* Parameters (in): ChannelId - ID of DIO channel.
+*                  Level - Value to be written.
+* Parameters (inout): None
+* Parameters (out): None
+* Return value: None
+* Description: Function to set a level of a Port.
+************************************************************************************/
+
+void Dio_WritePort(Dio_PortType PortId, Dio_PortLevelType Level)
+{
+	boolean error = FALSE;
+
+	/* Check if the Driver is initialized before using this function */
+	if (DIO_NOT_INITIALIZED == Dio_Status)
+	{
+		error = TRUE;
+	}
+
+	/* Check if the used channel is within the valid range */
+	if (DIO_CONFIGURED_CHANNLES <= ChannelId)
+	{
+		error = TRUE;
+	}
+
+	/* In-case there are no errors */
+	if(FALSE == error)
+	{
+		/* Point to the correct PORT register according to the Port Id stored in the Port_Num member */
+		switch(PortId)
+		{
+            case 0:    GPIO_PORTA_DATA_REG = Level;
+		               break;
+		    case 1:    GPIO_PORTB_DATA_REG = Level;
+		               break;
+		    case 2:    GPIO_PORTC_DATA_REG = Level;
+		               break;
+		    case 3:    GPIO_PORTD_DATA_REG = Level;
+		               break;
+            case 4:    GPIO_PORTE_DATA_REG = Level;
+		               break;
+            case 5:    GPIO_PORTF_DATA_REG = Level;
+		               break;
+		}
+	}
+}
+
+/************************************************************************************
+* Service Name: Dio_ReadPort
+* Sync/Async: Synchronous
+* Reentrancy: Reentrant
+* Parameters (in): ChannelId - ID of DIO channel.
+* Parameters (inout): None
+* Parameters (out): None
+* Return value: Dio_LevelType
+* Description: Function to return the value of the specified DIO Port.
+************************************************************************************/
+
+Dio_LevelType Dio_ReadPort(Dio_PortType PortId)
+{
+	Dio_PortLevelType output = STD_LOW;
+	boolean error = FALSE;
+
+	/* Check if the Driver is initialized before using this function */
+	if (DIO_NOT_INITIALIZED == Dio_Status)
+	{
+		error = TRUE;
+	}
+	
+	/* Check if the used channel is within the valid range */
+	if (DIO_CONFIGURED_CHANNLES <= ChannelId)
+	{
+		error = TRUE;
+	}
+
+	/* In-case there are no errors */
+	if(FALSE == error)
+	{
+		/* Point to the correct PORT register according to the Port Id stored in the Port_Num member */
+		switch(PortId)
+		{
+            case 0:    output = GPIO_PORTA_DATA_REG;
+		               break;
+		    case 1:    output = GPIO_PORTB_DATA_REG;
+		               break;
+		    case 2:    output = GPIO_PORTC_DATA_REG;
+		               break;
+		    case 3:    output = GPIO_PORTD_DATA_REG;
+		               break;
+            case 4:    output = GPIO_PORTE_DATA_REG;
+		               break;
+            case 5:    output = GPIO_PORTF_DATA_REG;
+		               break;
+		}
+	}
+        return output;
+}
